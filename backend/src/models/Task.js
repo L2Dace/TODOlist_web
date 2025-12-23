baedddd83e446b53438aca4fba72bb1266d52104
Duy <leducduydn.7122111@gmail.com>
2025-12-23 10:42:59 +0700

    CRUD api - done

---
+++ backend/src/models/Task.js
@@ -0,0 +1,26 @@
+import  mongoose, { Schema } from 'mongoose';
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            require: true,
+            trim: true,
+        },
+        status: {
+            type: String,
+            enum: ["active", "complete"],
+            default: "active",
+        },
+        completeAt: {
+            type: Date,
+            default: null
+        }
+    },
+    {
+        timestamps: true, //createAt và updateAt tự động thêm vào
+    }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
---
+++ db_pass.md
@@ -0,0 +1,5 @@
+MongoDB
+username: leducduy
+password: Blackace71202
+Connection String: mongodb+srv://leducduy:Blackace71202@cluster0.6lxpyqu.mongodb.net/?appName=Cluster0
+-----------------------------
