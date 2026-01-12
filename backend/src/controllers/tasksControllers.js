f5772549765efbcf32eed4f2a99f5040df5a556d
Duy <leducduydn.7122111@gmail.com>
2026-01-12 23:39:24 +0700

    DateTimeFilter

--- backend/src/controllers/tasksControllers.js
+++ backend/src/controllers/tasksControllers.js
@@ -1,13 +1,44 @@
 import Task from "../models/Task.js";
 
 export const getAllTasks = async (req, res) => {
+  const { filter = "today" } = req.query;
+  const now = new Date();
+  let startDate;
+
+  switch (filter) {
+    case "today": {
+      startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate()); //2026-01-12 00:00
+      break;
+    }
+    case "week": {
+      const mondayDate =
+        now.getDate() - (now.getDay() - 1) - (now.getDay() === 0 ? 7 : 0);
+      startDate = new Date(now.getFullYear(), now.getMonth(), mondayDate);
+      break;
+    }
+    case "month": {
+      startDate = new Date(now.getFullYear(), now.getMonth(), 1);
+      break;
+    }
+    case "all":
+    default: {
+      startDate = null;
+    }
+  }
+
+  const query = startDate ? { createdAt: { $gte: startDate } } : {};
+
   try {
     const result = await Task.aggregate([
+      { $match: query },
       {
         $facet: {
           tasks: [{ $sort: { createdAt: -1 } }],
           activeCount: [{ $match: { status: "active" } }, { $count: "count" }],
-          completeCount: [{ $match: { status: "complete" } }, { $count: "count" }],
+          completeCount: [
+            { $match: { status: "complete" } },
+            { $count: "count" },
+          ],
         },
       },
     ]);
@@ -16,7 +47,7 @@ export const getAllTasks = async (req, res) => {
     const activeCount = result[0].activeCount[0]?.count || 0;
     const completeCount = result[0].completeCount[0]?.count || 0;
 
-    res.status(200).json({tasks, activeCount, completeCount});
+    res.status(200).json({ tasks, activeCount, completeCount });
   } catch (error) {
     console.log("Lỗi khi gọi getAllTasks", error);
     res.status(500).json({ message: "Lỗi hệ thống" });
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,15 +14,16 @@ const HomePage = () => {
   const [activeTaskCount, setActiveTaskCount] = useState(0);
   const [completeTaskCount, setCompleteTaskCount] = useState(0);
   const [filter, setFilter] = useState("all");
+  const [dateQuery, setDateQuery] = useState('today');
 
   useEffect(() => {
     fetchTasks();
-  }, []);
+  }, [dateQuery]);
 
   //logic
   const fetchTasks = async () => {
     try {
-      const res = await api.get("/tasks");
+      const res = await api.get(`/tasks?filter=${dateQuery}`);
       setTaskBuffer(res.data.tasks);
       setActiveTaskCount(res.data.activeCount);
       setCompleteTaskCount(res.data.completeCount);
@@ -82,7 +83,7 @@ const HomePage = () => {
           {/* Phân trang và lọc theo ngày */}
           <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
             <TaskListPagination />
-            <DateTimeFilter />
+            <DateTimeFilter dateQuery={dateQuery} setDateQuery={setDateQuery}/>
           </div>
 
           {/* Chân trang */}
