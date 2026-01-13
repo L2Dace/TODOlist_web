5061d6c5cbce4ba728ebc49d8ec4f59dd067e5bc
Duy <leducduydn.7122111@gmail.com>
2026-01-13 11:18:50 +0700

    pagination

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,17 +8,23 @@ import TaskListPagination from "@/components/TaskListPagination";
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import api from "@/lib/axios";
+import { visibleTaskLimit } from "@/lib/data";
 
 const HomePage = () => {
   const [taskBuffer, setTaskBuffer] = useState([]);
   const [activeTaskCount, setActiveTaskCount] = useState(0);
   const [completeTaskCount, setCompleteTaskCount] = useState(0);
   const [filter, setFilter] = useState("all");
-  const [dateQuery, setDateQuery] = useState('today');
+  const [dateQuery, setDateQuery] = useState("today");
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetchTasks();
   }, [dateQuery]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [filter, dateQuery]);
 
   //logic
   const fetchTasks = async () => {
@@ -36,7 +42,21 @@ const HomePage = () => {
 
   const handleTaskChanged = () => {
     fetchTasks();
-  }
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) {
+      setPage((prev) => prev + 1);
+    }
+  };
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
 
   //biến
   const filteredTasks = taskBuffer.filter((task) => {
@@ -49,6 +69,17 @@ const HomePage = () => {
         return true;
     }
   });
+
+  const visibleTasks = filteredTasks.slice(
+    (page - 1) * visibleTaskLimit,
+    page * visibleTaskLimit
+  );
+
+  if (visibleTasks.length === 0) {
+    handlePrev();
+  }
+
+  const totalPages = Math.ceil(filteredTasks.length / visibleTaskLimit);
 
   return (
     <div className="min-h-screen w-full relative">
@@ -67,7 +98,7 @@ const HomePage = () => {
           <Header />
 
           {/* Tạo nhiệm vụ mới */}
-          <AddTask handleNewTaskAdded={handleTaskChanged}/>
+          <AddTask handleNewTaskAdded={handleTaskChanged} />
 
           {/* Thống kê và bộ lọc */}
           <StatsAndFilters
@@ -78,12 +109,22 @@ const HomePage = () => {
           />
 
           {/* Danh sách nhiệm vụ */}
-          <TaskList filteredTasks={filteredTasks} filter={filter} handleTaskChanged={handleTaskChanged}/>
+          <TaskList
+            filteredTasks={visibleTasks}
+            filter={filter}
+            handleTaskChanged={handleTaskChanged}
+          />
 
           {/* Phân trang và lọc theo ngày */}
           <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-            <TaskListPagination />
-            <DateTimeFilter dateQuery={dateQuery} setDateQuery={setDateQuery}/>
+            <TaskListPagination
+              handleNext={handleNext}
+              handlePrev={handlePrev}
+              handlePageChange={handlePageChange}
+              page={page}
+              totalPages={totalPages}
+            />
+            <DateTimeFilter dateQuery={dateQuery} setDateQuery={setDateQuery} />
           </div>
 
           {/* Chân trang */}
